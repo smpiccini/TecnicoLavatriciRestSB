42375b346f624d10240656371b98b3d961129118
samue <samue@192.168.1.234>
2022-06-21 14:24:57 +0200

    First commit

---
+++ src/main/java/com/acme/lavatriciRest/TecnicoLavatriciRestSbApplication.java
@@ -0,0 +1,13 @@
+package com.acme.lavatriciRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicoLavatriciRestSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicoLavatriciRestSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/lavatriciRest/TecnicoLavatriciRestSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.lavatriciRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicoLavatriciRestSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
