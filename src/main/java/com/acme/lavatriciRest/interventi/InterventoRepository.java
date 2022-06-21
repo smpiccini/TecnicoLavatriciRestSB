1092d2a62941027b7022d6d9002e6622d220f269
rosam <rosam@LAPTOP-J9NR809C>
2022-06-21 16:27:47 +0200

    repository

--- src/main/java/com/acme/lavatriciRest/interventi/InterventoRepository.java
+++ src/main/java/com/acme/lavatriciRest/interventi/InterventoRepository.java
@@ -1,8 +1,10 @@
 package com.acme.lavatriciRest.interventi;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InterventoRepository extends PagingAndSortingRepository<InterventoImp, Long> {
 
-	//ciao
+
 }
